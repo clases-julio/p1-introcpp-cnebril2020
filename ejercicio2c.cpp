a610e5ec14dfdd89bebefebe59b74190d7a86321
Carlos Nebril <carlosnj@f-l3209-pc16.aulas.etsit.urjc.es>
2023-02-15 12:17:38 +0100

    update

---
+++ ejercicio2c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main() {
+  char uno[] = "1 ";
+  char dos[] = "2 ";
+  char tres[] = "3 ";
+  char cuatro[] = "4 \n";
+
+  std::cout << uno;
+  std::cout << dos;
+  std::cout << tres;
+  std::cout << cuatro;
+
+
+  return 0;
+}
