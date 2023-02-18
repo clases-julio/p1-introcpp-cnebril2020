07485538662562aaf7d6b975a7cbcaf4ad030709
cnebril2020 <c.nebril.2020@alumnos.urjc.es>
2023-02-18 20:31:58 +0100

    hola

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main(){
+  char hola[1];
+  char adios[1];
+
+  std::cout << "Write a character: ";
+  std::cin >> hola;
+
+  std::cout << "Character:" << hola << std::endl;
+  std::cout << "Character2:" << adios << std::endl;
+}
+
+
