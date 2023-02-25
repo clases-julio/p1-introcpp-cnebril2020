2c5fa633372339654358bdc9d7b0b2197279eb69
cnebril2020 <c.nebril.2020@alumnos.urjc.es>
2023-02-25 14:22:04 +0100

    hola

---
+++ ejercicio6a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string cad1 = "* * * * * * * * ";
+  string cad2 = " * * * * * * * *";
+  
+  cout << cad1 << endl;
+  cout << cad2 << endl;
+  cout << cad1 << endl;
+  cout << cad2 << endl;
+  cout << cad1 << endl;
+  cout << cad2 << endl;
+  cout << cad1 << endl;
+  cout << cad2 << endl;
+
+  return 0;
+}
---
+++ ejercicio6b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {  
+  string cad = "* * * * * * * * \n * * * * * * * *";
+  
+  for (int i = 0; i < 4; ++i) {
+    cout << cad << endl;
+}
+
+  return 0;
+}
